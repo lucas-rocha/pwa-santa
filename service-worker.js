cadd6955f223587dbb4ee261e5c498d6fd5bdc8f
gabrielgouvertte <gfgouvertte@gmail.com>
2024-12-10 11:48:14 -0300

    Inicio PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('countries-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/',
+                '/',
+                '/',
+                '/'
+            ])
+        })
+    )
+})
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request)
+            })
+    )
+})
